b9faceba81683c1a29f57a493c9355daf714e380
sagartaneja <49398979+sagartaneja@users.noreply.github.com>
2020-04-15 20:41:56 -0700

    New IPhython Notebook Added

---
+++ Phython.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello, World!") # Print the traditional hello world
+
+
+# In[15]:
+
+
+type(12.0)
+
+
+# In[16]:
+
+
+type(4)
+
+
+# In[3]:
+
+
+type("Hello, Phython 101!")
+
+
+# In[17]:
+
+
+sys.float_info
+
+
+# In[18]:
+
+
+float(2)
+
+
+# In[19]:
+
+
+type(float(2))
+
+
+# In[20]:
+
+
+int(1.1)
+
+
+# In[21]:
+
+
+int('1')
+
+
+# In[22]:
+
+
+int('1 or 2 people')
+
+
+# In[23]:
+
+
+float('1.2')
+
+
+# In[24]:
+
+
+str(1)
+
+
+# In[25]:
+
+
+str(1.2)
+
+
+# In[27]:
+
+
+type(True)
+
+
+# In[28]:
+
+
+type(False)
+
+
+# In[29]:
+
+
+int(True)
+
+
+# In[30]:
+
+
+int(False)
+
+
+# In[31]:
+
+
+bool(1)
+
+
+# In[32]:
+
+
+bool(0)
+
+
+# In[33]:
+
+
+float(True)
+
+
+# In[4]:
+
+
+type(6/2) # float
+
+
+# In[5]:
+
+
+type(6//2) #int
+
+
+# In[6]:
+
+
+160/60
+
+
+# In[12]:
+
+
+total_min = 43 + 42 + 57 # Total length of albums in minutes
+total_min
+
+
+# In[13]:
+
+
+total_hours = total_min / 60 # Total length of albums in hours
+total_hours
+
+
+# In[19]:
+
+
+x = 3 + 2 * 2
+x
+
+
+# In[20]:
+
+
+y = (3 + 2) * 2
+y
+
+
+# In[21]:
+
+
+z = x + y
+z
+
+
+# In[ ]:
+
+
+
+
